6267c5f1cbdaf48f9508d74349eeefe3c5802e93
Yapanit572 <geraldharrya@gmail.com>
2026-01-09 19:31:29 +0800

    another testing

--- project/settings/base.py
+++ project/settings/base.py
@@ -87,7 +87,7 @@ USE_I18N = True
 USE_TZ = True
 
 # Static & Media
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
