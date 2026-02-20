eb05d3588259ef635e7c2c8895467895e65ad488
Yapanit572 <geraldharrya@gmail.com>
2026-02-21 00:06:38 +0800

    avoid queries

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,10 +1,11 @@
 """
 Django signals to auto-create and save Profile objects on User creation and update.
 """
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_migrate
 from django.dispatch import receiver
 from django.contrib.auth import get_user_model
 from .models import Profile
+import os
 
 User = get_user_model()
 
@@ -32,3 +33,16 @@ def save_user_profile(sender, instance, created=False, **kwargs):
         instance.profile.save()
     except Profile.DoesNotExist:
         Profile.objects.get_or_create(user=instance)
+
+
+@receiver(post_migrate)
+def ensure_superuser(sender, **kwargs):
+    try:
+        username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+        email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+        password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+        if username and email and password:
+            if not User.objects.filter(username=username).exists():
+                User.objects.create_superuser(username=username, email=email, password=password)
+    except Exception:
+        pass
