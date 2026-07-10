232754da7b1b5327ee99f95d090645bc7cea01ed
kleocayrel821 <claycyrl0821@gmail.com>
2026-07-10 13:58:43 +0800

    sensor

---
+++ controller/migrations/0020_merge_0019_branches.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("controller", "0019_alter_feedingschedule_portion_size_and_more"),
+        ("controller", "0019_devicestatus_food_low_and_more"),
+    ]
+
+    operations = []
